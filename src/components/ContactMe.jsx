08b24ee4b3b4b8e663b250bfca8166815b884295
dusklight00 <r.rahul.developer@gmail.com>
2024-01-19 09:16:42 +0530

    contact form with formspree integration done

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -13,7 +13,11 @@ function ContactMe() {
           Got a technical issue? Want to talk about a certain anime? Collaborate
           on your next project? Let me know.
         </p>
-        <form action="#" class="space-y-8">
+        <form
+          action="https://formspree.io/f/xnqenrvl"
+          class="space-y-8"
+          method="POST"
+        >
           <div>
             <label
               for="email"
@@ -24,6 +28,7 @@ function ContactMe() {
             <input
               type="email"
               id="email"
+              name="email"
               class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="name@gmail.com"
               required
@@ -39,6 +44,7 @@ function ContactMe() {
             <input
               type="text"
               id="subject"
+              name="subject"
               class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="Let us know how we can help you"
               required
@@ -53,6 +59,7 @@ function ContactMe() {
             </label>
             <textarea
               id="message"
+              name="message"
               rows="6"
               class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder="Leave a comment..."
@@ -64,7 +71,7 @@ function ContactMe() {
           >
             Send message
           </button> */}
-          <Button>Send message</Button>
+          <Button type="submit">Send message</Button>
         </form>
       </div>
     </div>
